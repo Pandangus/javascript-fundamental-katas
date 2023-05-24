385dde5b6cee14010b668a177598f1635aa11f55
Angus <angushirst@outlook.com>
2023-05-24 22:47:27 +0100

    having another look

--- update-remote-students/update-remote-students.js
+++ update-remote-students/update-remote-students.js
@@ -1,6 +1,10 @@
 // Please do not change the name of this function
-function updateRemoteStudents (str) {
-  // Your code here
+function updateRemoteStudents (arrOfObj) {
+  return arrOfObj.map((object) => ({...object})).map((object) => {
+    if(!object.hasOwnProperty("location")){
+      object.location = "remote"
+  }
+  return object;
+});
 }
-
 module.exports = updateRemoteStudents;
