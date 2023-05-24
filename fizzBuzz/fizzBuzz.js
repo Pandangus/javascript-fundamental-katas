5382b78e25990175172d37df21262e8869e2bc47
Angus <angushirst@outlook.com>
2023-05-24 22:48:14 +0100

    adding the rest

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,11 +1,11 @@
-function fizzBuzz(num){
-    let total = 0;
-    for(let i = 0; i < num; i++){
-        if(i % 5 === 0 || i % 3 === 0){
-            total += i;
-        }
+function fizzBuzz(num) {
+  let total = 0;
+  for (let i = 0; i < num; i++) {
+    if (i % 5 === 0 || i % 3 === 0) {
+      total += i;
     }
-    return total;
+  }
+  return total;
 }
 
 module.exports = fizzBuzz;
