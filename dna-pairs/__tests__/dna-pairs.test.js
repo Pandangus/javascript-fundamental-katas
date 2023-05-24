8e6ae23db06247aaad6866caf8f20662ed1e8935
Angus <angushirst@outlook.com>
2023-05-24 14:36:23 +0100

    dna pairs

--- dna-pairs/__tests__/dna-pairs.test.js
+++ dna-pairs/__tests__/dna-pairs.test.js
@@ -1,11 +1,36 @@
-const dnaPairs = require('../dna-pairs.js');
+const dnaPairs = require("../dna-pairs.js");
 
-describe('dnaPairs()', () => {
-  test('returns an single array nested within an array when passed an empty string', () => {
+describe("dnaPairs()", () => {
+  test("returns an single array nested within an array when passed an empty string", () => {
     //arrange
-    const testInput = 
+    const testInput = "";
+    const expectedOutput = [[]];
     //act
-
+    const result = dnaPairs(testInput);
     //assert
+    expect(result).toEqual(expectedOutput);
+  });
+  test("returns correct match for single DNA character", () => {
+    //arrange
+    const testInput = "A";
+    const expectedOutput = [["A", "T"]];
+    //act
+    const result = dnaPairs(testInput);
+    //assert
+    expect(result).toEqual(expectedOutput);
+  });
+  test("returns correct match for multiple DNA characters", () => {
+    //arrange
+    const testInput = "ATGC";
+    const expectedOutput = [
+      ["A", "T"],
+      ["T", "A"],
+      ["G", "C"],
+      ["C", "G"],
+    ];
+    //act
+    const result = dnaPairs(testInput);
+    //assert
+    expect(result).toEqual(expectedOutput);
   });
 });
--- dna-pairs/dna-pairs.js
+++ dna-pairs/dna-pairs.js
@@ -1,6 +1,32 @@
 // Please do not change the name of this function
 function dnaPairs(dna) {
-  // Write your code here
+  if(dna === ""){
+    return [[]];
+  }
+
+  const newArr = [];
+
+  for(let i = 0; i < dna.length; i++){
+    switch(dna[i]){
+      case "A":
+        newArr.push(["A", "T"]);
+        break;
+
+      case "T":
+        newArr.push(["T", "A"]);
+        break;
+
+      case "G":
+         newArr.push(["G", "C"]);
+         break;
+
+      case "C":
+         newArr.push(["C", "G"]);
+         break;
+
+    }
+  }
+  return newArr;
 }
 
 module.exports = dnaPairs;
