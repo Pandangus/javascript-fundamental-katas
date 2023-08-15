2b4e21c0cd6457e2707db44edff17779afd998d6
Angus <angushirst@outlook.com>
2023-08-15 18:37:31 +0100

    test for words beginning with vowel

--- __tests__/pig-latin.test.js
+++ __tests__/pig-latin.test.js
@@ -1,7 +1,10 @@
 const pigLatin = require('../pig-latin/pig-latin.js')
 
 describe("pigLatin", () => {
-    test("should add 'way' as suffix to single vowel passed in", () => {
+    test("should add 'way' as suffix to single vowel", () => {
         expect(pigLatin("a")).toBe("away");
+    })
+    test("should add 'way' as suffix to a word beginning with a vowel", () => {
+        expect(pigLatin("apple")).toBe("appleway");
     })
 });
