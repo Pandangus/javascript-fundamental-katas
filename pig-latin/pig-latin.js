8bfa8a0b702bd6a754e4ad310b19a38b21961a63
Angus <angushirst@outlook.com>
2023-08-16 15:06:57 +0100

    removed redunant break statement

--- pig-latin/pig-latin.js
+++ pig-latin/pig-latin.js
@@ -20,7 +20,6 @@ function pigLatin(str) {
           count += 1;
           if(count == word.length){
             latinArr.push(`${word}ay`)
-            break
           }
         }
       }
@@ -43,26 +42,3 @@ function isVowel(inputStr) {
 }
 
 module.exports = pigLatin;
-
-/*
-define a new array
-split the sentance on the spaces
-check if first letter of each word begins with a vowel
-if yes: 
-  add 'way' to the end of word
-  push word to new array
-if no: 
-  iterate through the characters until you find a vowel
-    then: 
-      splice the beginning of the word
-      add it to end of word
-      add 'ay' to the end of word
-      push word to new array
-join new array
-
-To translate to Pig Latin you take the first 
-consonant (or consecutive consonants) of each ]
-word, move it to the end and also append 'ay'. 
-If the word starts with a vowel, just append 
-'way' at the end.
-*/
