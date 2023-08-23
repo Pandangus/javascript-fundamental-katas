6acc0b0c7ec658e575f08da605214ced4233f749
Angus <angushirst@outlook.com>
2023-08-23 14:46:34 +0100

    missing semicolon added

--- rotate-array/rotate-array.js
+++ rotate-array/rotate-array.js
@@ -1,7 +1,7 @@
 // Please do not change the name of this function
 function rotateArray(arr, num) {
-  arrCopy = [...arr]
-  rotatedNums = arrCopy.splice(-num % arr.length);
+  const arrCopy = [...arr];
+  const rotatedNums = arrCopy.splice(-num % arr.length);
   return rotatedNums.concat(arrCopy);
 }
 
