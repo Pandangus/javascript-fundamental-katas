92ba42242848e093ae82fe2c49266b7bec1f2080
Angus <angushirst@outlook.com>
2023-08-23 13:46:27 +0100

    test suite added for rotateArray

--- __tests__/rotate-array.test.js
+++ __tests__/rotate-array.test.js
@@ -1,10 +1,24 @@
 const rotateArray = require("../rotate-array/rotate-array");
 
 describe("rotateArray", () => {
-    test(`should return array unchanged if passed 0`, () => {
-      expect(rotateArray([1,2,3], 0)).toEqual([1,2,3]);
-    });
-    test(`should rotate array once to the right if passed 1`, () => {
-        expect(rotateArray([1,2,3], 1)).toEqual([3,1,2]);
-      });
+  test(`should return array unchanged if passed 0`, () => {
+    expect(rotateArray([1, 2, 3], 0)).toEqual([1, 2, 3]);
+  });
+  test(`should rotate to the right if passed an integer less than array length`, () => {
+    expect(rotateArray([1, 2, 3, 4], 1)).toEqual([4, 1, 2, 3]);
+  });
+  test(`should rotate to the left if passed a negative integer less than negative array length`, () => {
+    expect(rotateArray([1, 2, 3, 4], -2)).toEqual([3, 4, 1, 2]);
+  });
+  test(`should rotate array right if passed positive integer greater than array length`, () => {
+    expect(rotateArray([1, 2, 3, 4], 11)).toEqual([2, 3, 4, 1]);
+  });
+  test(`should rotate array left if passed negative integer lesser than negative array length`, () => {
+    expect(rotateArray(["A", 1, "🌼", "orange"], -6)).toEqual(["🌼", "orange", "A", 1]);
+  });
+  test(`should not mutate input`, () => {
+    input = [0, 9, 10];
+    output = rotateArray(input);
+    expect(output).not.toBe(input);
+  });
 });
--- rotate-array/rotate-array.js
+++ rotate-array/rotate-array.js
@@ -1,7 +1,7 @@
 // Please do not change the name of this function
 function rotateArray (arr, num) {
 
-  return arr.map
+  return arr
 };
 
 module.exports =  rotateArray;
