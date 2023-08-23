7a36d627aed93963c2862e1be5c7bb789fcb156e
Angus <angushirst@outlook.com>
2023-08-23 20:15:13 +0100

    codebody of heardTheBabies restored

--- herd-the-babies/herd-the-babies.js
+++ herd-the-babies/herd-the-babies.js
@@ -1,4 +1,9 @@
 // Please do not change the name of this function
-function herdTheBabies(str) {}
+function herdTheBabies(str) {
+  return str
+    .split("")
+    .sort((a, b) => a.localeCompare(b, "en-UK", { caseFirst: "upper" }))
+    .join("");
+}
 
 module.exports = herdTheBabies;
